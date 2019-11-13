a7084d2fe21cf9f0e60f188021b7cfbeb804fd5b
Marco Christiani <mchristiani2017@gmail.com>
2019-11-13 13:50:20 -0500

    ↑Code coverage and tests

--- test_main.py
+++ test_main.py
@@ -1,28 +1,49 @@
 from unittest import TestCase
 from Main import *
 
-VERBOSE = True
+VERBOSE = False
 
 
 class TestMain(TestCase):
-
-    def test_encrypt_128(self):
+    def test_encrypt_128_ecb(self):
         seed = '12476278dbc36bd9dc2cf5716a43b4bb'
         ptext = 'A01478BE92570366F1D13C098726DAC53722ddf33b549fb4d84da20855ed45bc'
         ctext = '196a288618c68aac46b475c66783929aacb12039b1fc7223e7f438e7ba354e19'
         result = encrypt(seed, ptext, verbose=VERBOSE)
         self.assertEqual(ctext, result)
-        print('-'*40)
 
+    def test_encrypt_128_cbc(self):
+        seed = '12476278dbc36bd9dc2cf5716a43b4bb'
+        ptext = 'A01478BE92570366F1D13C098726DAC53722ddf33b549fb4d84da20855ed45bcA01478BE92570366F1D13C098726DAC53722' \
+                'ddf33b549fb4d84da20855ed45bc'
+        ctext = '196a288618c68aac46b475c66783929a62317e77aed691f908680fc692d1ebec2cbccb9460b6a07bb932f18f65aab89dcf29' \
+                '662dd67d683af8904b2e242a2c0c'
+        result = encrypt(seed, ptext, BlockMode.CBC, verbose=VERBOSE)
+        self.assertEqual(ctext, result)
 
-
-    def test_encrypt_192(self):
+    def test_encrypt_192_ecb(self):
         seed = '9d1e29e03b24b556c16744b9fd5ba204b24b9d1e29e056c1'
 
         ptext = 'A01433b54978BE925703666F1D13C098726DAC56F1D13C098726DAC53722ddf33b549fb4A01478BE9257033722ddffb4'
         ctext = '566b9baab48dec6d53a34306bf54643d764776da4b6ac073d09a57ba55ee1b0d8a06f09a0dea5542e336fdd88de64a0d'
         result = encrypt(seed, ptext, verbose=VERBOSE)
         self.assertEqual(ctext, result)
+
+    def test_encrypt_192_cbc(self):
+        seed = '9d1e29e03b24b556c16744b9fd5ba204b24b9d1e29e056c1'
+
+        ptext = 'A01478BE92570366F1D13C098726DAC53722ddf33b549fb4d84da20855ed45bcA01478BE92570366F1D13C098726DAC53722' \
+                'ddf33b549fb4d84da20855ed45bc'
+        ctext = 'b549204a81419dbef1e439ffb20269cf3d5bd54dc704374e1ac6a752342450351c7f7d702b3f840bd7bebd2d691019b720c6' \
+                'a19be88415123ce03bac7a0ae660'
+        result = encrypt(seed, ptext, BlockMode.CBC, verbose=VERBOSE)
+        self.assertEqual(ctext, result)
+
+    def test_encrypt_256_ecb(self):
+        return NotImplemented
+
+    def test_encrypt_256_cbc(self):
+        return NotImplemented
 
     # def test_decrypt_128(self):
     #     seed = '12476278dbc36bd9dc2cf5716a43b4bb'
